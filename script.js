032101147564d3755866ab7a084b45eb28081455
Dmitriy-Zelentsov <zelentsov.dmitry@gmail.com>
2023-10-05 13:27:33 +0300

    ADD HW3

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// Задание 1 Создайте функцию которая возводит переданное число в куб, необходимо вывести в консоль результат 2^3 степени + 3 ^ 3 степени
 
-const cubingNumber = (number) => number ** 3;
-const number1 = 2;
-const number2 = 3;
-console.log(
-  `Сумма чисел ${number1} в кубе и ${number2} в кубе равна ${
-    cubingNumber(number1) + cubingNumber(number2)
-  }`
-);
+// Задача №1
+console.log('Задача № 1');
+function getCube(number) {
+  return number ** 3;
+} 
+
+let num = Number(prompt('Введите любое целое число', 5));
+console.log(num);
+let res = getCube(num);
+console.log(res);
+alert(`Куб числа ${num} равен ${res}`)
+
+
+// Задача №2
+console.log('Задача № 2')
+function getSalary(finance) {
